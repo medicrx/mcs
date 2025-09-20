d9e5ecd44bf9830eb2bc98aabd53c41f22e8c919
EDEMESSI Florian <florianedem@gmail.com>
2025-09-20 18:35:55 +0100

    update service-worker

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
     prefix: 'mcs',
-    suffix: 'v1.0',
+    suffix: 'v1.1', // Updated version for cache invalidation
     precache: 'precache',
     runtime: 'runtime-cache'
 });
@@ -17,23 +17,43 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest);
 // use `NetworkFirst` strategy for html
 workbox.routing.registerRoute(
     /\.html$/,
-    new workbox.strategies.NetworkFirst()
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'html-cache',
+        networkTimeoutSeconds: 3
+    })
 );
 
-// use `NetworkFirst` strategy for css and js
+// use `CacheFirst` strategy for css and js (static assets)
 workbox.routing.registerRoute(
     /\.(?:js|css)$/,
-    new workbox.strategies.NetworkFirst()
+    new workbox.strategies.CacheFirst({
+        cacheName: 'static-assets',
+        plugins: [{
+            cacheKeyWillBeUsed: async ({ request }) => {
+                return `${request.url}?v=${new Date().getTime()}`;
+            }
+        }]
+    })
 );
 
 // use `CacheFirst` strategy for images
 workbox.routing.registerRoute(
-    /assets\/(img|icons)/,
-    new workbox.strategies.CacheFirst()
+    /assets\/(img|icons|images)/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'image-cache',
+        plugins: [{
+            cacheExpiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+            }
+        }]
+    })
 );
 
-// use `StaleWhileRevalidate` third party files
+// use `StaleWhileRevalidate` for third party files
 workbox.routing.registerRoute(
-    /^https?:\/\/cdn.staticfile.org/,
-    new workbox.strategies.StaleWhileRevalidate()
+    /^https?:\/\/(cdn\.staticfile\.org|fonts\.googleapis\.com|pro\.fontawesome\.com)/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'third-party-cache'
+    })
 );
